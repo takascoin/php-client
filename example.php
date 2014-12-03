1224c5c5621e4a62fa4f904f423e06e89cb00801
paddlesteamer <umt.erdl@gmail.com>
2014-12-03 17:40:22 +0100

    parameter fix

--- example.php
+++ example.php
@@ -2,10 +2,10 @@
 
 	require_once('./takascoin.php');
 
-	function createPayment($amount, $apiKey) {
+	function createPayment($amount, $apiKey, $currency) {
 		$takas = new Takascoin();
 
-		$payment = $takas->payment($amount, $apiKey);
+		$payment = $takas->payment($amount, $apiKey, array( 'currency' => $currency ));
 
 		var_dump($payment);
 
@@ -16,20 +16,20 @@
 		}
 	}
  
-	function getButton($amount, $apiKey) {
+	function getButton($amount, $apiKey, $currency) {
 		$takas = new Takascoin();
 
-		$button = $takas->button($amount, $address);
+		$button = $takas->button($amount, $address, array( 'currency' => $currency ));
 
 		var_dump($button);
 	}
 	
 	
-	$amount        = 0.0005;
-	$apiKey       = "{Takas_merchant_email}";
-	$currency      = "BTC"; // or 'TRY'
+	$amount   = 0.0005;
+	$apiKey   = "{Takas_merchant_email}";
+	$currency = "BTC"; // or 'TRY'
 
-	//createPayment($amount, $currency, $address);
-	//getButton($amount, $currency, $address);
+	//createPayment($amount, $apiKey, $currency);
+	//getButton($amount, $apiKey, $currency);
 
 ?>
